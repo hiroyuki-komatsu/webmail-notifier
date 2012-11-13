e5987b298aa2403ec40fd45d531eb9d88397ffe3
Hiroyuki Komatsu <hiroyuki.komatsu@gmail.com>
2012-11-13 23:44:01 +0900

    Add files for unit test with the GoogleTest library.

---
+++ buildorb_test/buildorb_test.mm
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  buildorb_test
+//
+//  Created by Hiroyuki Komatsu on 2012/11/12.
+//
+//
+
+// #import <Foundation/Foundation.h>
+
+#include "gtest/gtest.h"
+
+TEST(BuildOrbTest, HelloWorld) {
+  EXPECT_TRUE(true);
+}
